cdbc8a8af9771ea17b153a466662d19d35caa697
Isaac Arnold <isaacarnold@icloud.com>
2020-09-23 21:07:16 +1200

    Final solution for search filter found.

--- js/searchPhotos.js
+++ js/searchPhotos.js
@@ -1,30 +1,13 @@
-
-// function searchPhotos() {
-
-//     let input = document.getElementById("#search").value;
-//     let filter = input.value.toUpperCase();
-//     let textValue;
-//     let caption = document.querySelectorAll(".gallery-container a");
-
-//     for (i = 0; i < caption.length; i++) {
-//         a = caption[i].getAttribute("data-caption")[0];
-//         textValue = caption.textContent || caption.innerText;
-//         if (textValue.toUpperCase().includes(filter) > -1) {
-//             caption[i].style.display = "block";
-//         } else {
-//             caption[i].style.display = "none";
-//         }
-//     }
-// }
+// JavaScript credit/idea from itsmeganlynn's GitHub repo: techdegree-project-5
 
 function searchPhotos() {
-    let input = document.querySelector("#search").value;
-    let caption = document.querySelectorAll(".gallery-container a");
+    let input = document.querySelector("#search").value; // This grabs the value typed into the search field
+    let caption = document.querySelectorAll(".gallery-container a"); // This grabs the images within the gallery container
     for (let i = 0; i < caption.length; i++) {
        if (caption[i].getAttribute("data-caption").toLowerCase().includes(input.toLowerCase())) {
            caption[i].style.display = "";
        } else {
-           caption.style.display = "none";
+           caption[i].style.display = "none";
        }
     }
 }
