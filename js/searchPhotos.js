96e9eea88f6105867d4e802c75f2b08a9d58750e
Isaac Arnold <isaacarnold@icloud.com>
2020-09-23 20:59:09 +1200

    Tried a different version of search filter

--- js/searchPhotos.js
+++ js/searchPhotos.js
@@ -1,18 +1,30 @@
 
+// function searchPhotos() {
+
+//     let input = document.getElementById("#search").value;
+//     let filter = input.value.toUpperCase();
+//     let textValue;
+//     let caption = document.querySelectorAll(".gallery-container a");
+
+//     for (i = 0; i < caption.length; i++) {
+//         a = caption[i].getAttribute("data-caption")[0];
+//         textValue = caption.textContent || caption.innerText;
+//         if (textValue.toUpperCase().includes(filter) > -1) {
+//             caption[i].style.display = "block";
+//         } else {
+//             caption[i].style.display = "none";
+//         }
+//     }
+// }
+
 function searchPhotos() {
-
-    let input = document.getElementById("search");
-    let filter = input.value.toUpperCase();
-    let textValue;
+    let input = document.querySelector("#search").value;
     let caption = document.querySelectorAll(".gallery-container a");
-
-    for (i = 0; i < caption.length; i++) {
-        a = caption[i].getAttribute("data-caption")[0];
-        textValue = caption.textContent || caption.innerText;
-        if (textValue.toUpperCase().includes(filter) > -1) {
-            caption[i].style.display = "block";
-        } else {
-            caption[i].style.display = "none";
-        }
+    for (let i = 0; i < caption.length; i++) {
+       if (caption[i].getAttribute("data-caption").toLowerCase().includes(input.toLowerCase())) {
+           caption[i].style.display = "";
+       } else {
+           caption.style.display = "none";
+       }
     }
 }
